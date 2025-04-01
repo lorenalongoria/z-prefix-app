ee45db18dd23b61ab609365b8d196974d0acdc01
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-01 15:07:41 -0400

    users.js - signup for an account & login to account

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const knex = require('../db');
+const knex = require("../db");
 
+// Story #1 - create an account
+router.post("/register", async (req, res) => {
+  const { first_name, last_name, username, password } = req.body;
+  const [newUser] = await knex("users")
+    .insert({ first_name, last_name, username, password })
+    .returning("*");
+  res.status(201).json(newUser);
+});
+
+// Story #2 - login to an account
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await knex("users").where({ username, password }).first();
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-const useRoutes = require('./routes/users');
-const itemRoutes = require('./routes/items');
+const useRoutes = require("./routes/users");
+const itemRoutes = require("./routes/items");
 
-app.use('/api/users', useRoutes);
-app.use('/api/items', itemRoutes);
+app.use("/api/users", useRoutes);
+app.use("/api/items", itemRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
