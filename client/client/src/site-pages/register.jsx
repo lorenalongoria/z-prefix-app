fb51aba4c15616e5a5702103f96275c7916a8762
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-01 17:07:38 -0400

    login page attempt 1 (front end is down currently)

--- client/client/src/site-pages/register.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export default function Register() {
-  const [userData, setUserData] = useState({
-    first_name: '',
-    last_name: '',
-    username: '',
-    password: '',
-  });
