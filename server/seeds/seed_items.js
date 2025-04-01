c40f1b869bc5d221e62acc8b865fff3a4421728d
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-01 18:32:02 -0400

    console.logs for troubleshooting - trying to distribute items to users

--- server/seeds/seed_items.js
+++ server/seeds/seed_items.js
@@ -7,16 +7,28 @@ exports.seed = async function (knex) {
   await knex("items").del();
 
   try {
+    console.log("Fetching products from DummyJSON...");
     const response = await fetch("https://dummyjson.com/products");
     const data = await response.json();
+    console.log("Fetched data:", data);
+    if (!data.products || data.products.length === 0) {
+      console.log("No products found in the API response.");
+      return;
+    }
+    console.log(`Fetched ${data.products.length} products.`);
+
+    const minUserId = 2;
+    const maxUserId = 31;
 
     const items = data.products.map((product) => ({
-      user_id: 1,
+      user_id:
+        Math.floor(Math.random() * (maxUserId - minUserId + 1)) + minUserId,
       item_name: product.title,
       description: product.description,
       quantity: product.stock,
     }));
 
+    console.log("Inserting", items.length, "items into the database...");
     await knex("items").insert(items);
     console.log("Items seeded successfully.");
   } catch (error) {
