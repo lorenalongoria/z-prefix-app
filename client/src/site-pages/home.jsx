3f9ab204d74d2ceb474a0632fe8f36ee78d1a15f
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-02 11:37:30 -0400

    favicon + home page css updates

--- client/src/site-pages/home.jsx
+++ client/src/site-pages/home.jsx
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
-      <h1>Inventory Management System</h1>
-      <br></br>
-      <Link to={"/register"}>
-        <button>Register</button>
-      </Link>
+      <h1>Online Store Database</h1>
+      <h2>Inventory Managers:</h2>
+      <p>To view or edit items, please login or create an account.</p>
       <Link to={"/login"}>
         <button>Login</button>
       </Link>
+      <Link to={"/register"}>
+        <button>Register</button>
+      </Link>
+      <h2>Visitors:</h2>
+      <p>To view all items, please select the button below.</p>
       <Link to={"/inventory"}>
         <button>View Inventory</button>
       </Link>
